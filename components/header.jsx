51df867ead5f63fa11bac52e4faaef91ccc352e3
TopKiller228 <116894533+TopKiller228@users.noreply.github.com>
2023-03-03 18:00:06 +0300

    first_comit
    Для всех

---
+++ components/header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return(
+        <header>
+            <div>
+                <span className="logo">Eclips Shop</span>
+            </div>
+
+            <div className="presentation"></div>    
+       </header>
+    )
+}
---
+++ index.jsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header"
+import { Footer } from "@/components/footer"
+
+const Home = () => {
+    return(
+      <div className="qwerty">
+          <Header />
+          <Footer />
+      </div>
+    )
+}
+
+export default Home
